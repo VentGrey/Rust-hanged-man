a8aaae617aa8dcb2bd87a5565254a30610f66020
VentGrey <omarpurataf@gmail.com>
2018-11-03 18:10:24 -0600

    función para abrir el archivo

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() {
 
+//La función usar palabra, no toma ningún argumento y retorna una cadena.
+fn usar_palabra() -> String {
+    //abrir archivo
+    let mut archivo = File::open("palabras.txt").expect("No se pudo abrir el archivo");
 }
+
+fn main() {
+    let palabra_usada = usar_palabra();
+}
+
+
